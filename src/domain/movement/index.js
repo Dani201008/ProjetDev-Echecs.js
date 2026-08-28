77e1030b38d22a7fb30f6888f2f84ccc6538087a
SamuelTheytaz <samuel.thyz@icloud.com>
2026-08-28 10:57:31 +0200

    feat(moteur): cavalier, roi, pion, table de dispatch et génération des coups légaux

--- src/domain/movement/index.js
+++ src/domain/movement/index.js
@@ -1,3 +1,22 @@
 /**
- * movement/index.js : la table reliant chaque pièce à ses coups.
+ * movement/index.js
+ * Associe chaque type de pièce à sa fonction de déplacement
+ * Auteur : Samuel Theytaz
+ * Date : 27.08.2026
  */
+import { PieceType } from '../piece.js'
+import * as pawn from './pawn.js'
+import * as knight from './knight.js'
+import * as bishop from './bishop.js'
+import * as rook from './rook.js'
+import * as queen from './queen.js'
+import * as king from './king.js'
+
+export const MOVE_GENERATORS = Object.freeze({
+  [PieceType.PAWN]: pawn.pseudoLegalMoves,
+  [PieceType.KNIGHT]: knight.pseudoLegalMoves,
+  [PieceType.BISHOP]: bishop.pseudoLegalMoves,
+  [PieceType.ROOK]: rook.pseudoLegalMoves,
+  [PieceType.QUEEN]: queen.pseudoLegalMoves,
+  [PieceType.KING]: king.pseudoLegalMoves,
+})
--- src/domain/movement/king.js
+++ src/domain/movement/king.js
@@ -1,3 +1,30 @@
 /**
- * movement/king.js : les coups du roi.
+ * movement/king.js
+ * Déplacements du roi sur les cases voisines, le roque est traité dans moveGenerator.js
+ * Auteur : Samuel Theytaz
+ * Date : 27.08.2026
  */
+import { createSquare, isInside } from '../square.js'
+import { createMove, MoveFlag } from '../move.js'
+import { pieceAt } from '../position.js'
+
+const OFFSETS = [
+  [1, 0], [-1, 0], [0, 1], [0, -1],
+  [1, 1], [1, -1], [-1, 1], [-1, -1],
+]
+
+// Coups pseudo-légaux du roi depuis la case from, hors roque
+export function pseudoLegalMoves(position, from) {
+  const moves = []
+  const me = pieceAt(position, from)
+  for (const [df, dr] of OFFSETS) {
+    const file = from.file + df
+    const rank = from.rank + dr
+    if (!isInside(file, rank)) continue
+    const target = createSquare(file, rank)
+    const occupant = pieceAt(position, target)
+    if (!occupant) moves.push(createMove(from, target))
+    else if (occupant.color !== me.color) moves.push(createMove(from, target, { flag: MoveFlag.CAPTURE }))
+  }
+  return moves
+}
